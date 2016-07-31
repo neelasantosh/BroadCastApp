c3d7598d245a740d44ef415a437b444f09f64103
neelasantosh <neela.santosh2405@gmail.com>
2016-07-31 14:57:52 +0530

    BroadCast

---
+++ src/com/example/broadcastapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.broadcastapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	
+	
+	EditText editMessage;
+	Button buttonSend;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editMessage = (EditText) findViewById(R.id.editText1);
+        buttonSend = (Button) findViewById(R.id.button1);
+        buttonSend.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				String str = editMessage.getText().toString();
+				Intent in = new Intent();
+				in.putExtra("data", str);
+				
+				in.setAction("com.example.broadcastapp.ACTION_DATA");
+				sendBroadcast(in);//-->OS
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
